13e7abbddc189daa03e7394d6949b64148803af7
BlackAngel52 <151714758+BlackAngel52@users.noreply.github.com>
2025-07-15 22:37:48 +0300

    Add files via upload

---
+++ netology_task_5_2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main()
+{
+    const int arr_len{ 10 };
+    int arr[arr_len]{1,500,3,4,-50,6,7,8,9,10};
+    int count{};
+    int min{10000};
+    int max{-10000};
+
+
+    for (count; count < arr_len; ++count) {
+        std::cout << arr[count] << " ";
+        if (arr[count] < min) {
+            min = arr[count];
+        }
+        if (arr[count] > max) {
+            max = arr[count];
+        }
+    }
+    std::cout << std::endl;
+    std::cout << "Min = " << min << " Max = " << max << std::endl;
+
+}
+
