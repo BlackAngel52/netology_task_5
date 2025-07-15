97fa98e2eaccd86131578e787d92de806d908f38
BlackAngel52 <151714758+BlackAngel52@users.noreply.github.com>
2025-07-15 22:28:11 +0300

    Add files via upload

---
+++ netology_task_5_1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    const int arr_len{ 10 };
+    int arr[arr_len]{};
+    int count{};
+
+    for (count; count < arr_len-1; ++count) {
+        arr[count] = count;
+        std::cout << arr[count] << ", ";
+    }
+    arr[count] = count;
+    std::cout << arr[count];
+
+}
+
